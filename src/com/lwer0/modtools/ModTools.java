b39621f534ef6f68c0545f4e0c3c0ec01809d861
Joel <Joel@192.168.1.177>
2016-06-18 19:55:01 +0200

    claro que si

--- src/com/lwer0/modtools/ModTools.java
+++ src/com/lwer0/modtools/ModTools.java
@@ -9,7 +9,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ModTools extends JavaPlugin {
     
-    //Enable2
+    //Enabling
     @Override
     public void onEnable() {
         this.getLogger().info("ModTools enabled correctly");
